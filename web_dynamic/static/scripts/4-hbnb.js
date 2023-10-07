6a7d553f388ae726ff6b8e98f11b7d911d100096
wughangar <wughangar.rose@gmail.com>
2023-10-07 03:23:23 +0300

    added button

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+	const selectedAmenities = [];
+
+	function updateSelectedAmenities() {
+		$("#selected-amenities-list").text(selectedAmenities.join(", "));
+	}
+	$("input[type='checkbox']").change(function() {
+		const amenityId = $(this).data("id");
+		const amenityName = $(this).data("name");
+
+		 if ($(this).prop("checked")) {
+			 selectedAmenities.push(amenityName);
+		 } else {
+			 selectedAmenities = selectedAmenities.filter(function(name) {
+				 return name !== amenityName;
+			 });
+		 }
+		updateSelectedAmenities();
+	});
+	function checkApiStatus() {
+		$.get("http://0.0.0.0:5001/api/v1/status/", function(data) {
+			if (data.status === "OK") {
+				 $("#api_status").addClass("available");
+			} else {
+				$("#api_status").removeClass("available");
+			}
+		});
+	}
+
+	function fetchPlaces() {
+		$.ajax({
+			type: "POST",
+			url: "http://0.0.0.0:5001/api/v1/places_search",
+			contentType: "application/json",
+			data: JSON.stringify({}),
+			success: function(data) {
+				const placesSection = $("section.places");
+				placesSection.empty();
+
+				data.forEach(function(place) {
+					const article = $("<article>");
+					article.append($("<h2>").text(place.name));
+					rticle.append($("<div>").text(place.description));
+					placesSection.append(article);
+				});
+			},
+			error: function() {
+				console.error("Error fetching places data.");
+			}
+		});
+	}
+
+	checkApiStatus();
+	fetchPlaces();
+
+	$("button").click(function() {
+		searchPlacesByAmenities();
+	});
+});
